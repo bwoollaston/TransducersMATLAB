c9517670b514e67ec9dd6f671d67ba1c14a69f26
Brodie <cwoollas@gmail.com>
2023-02-08 10:08:27 -0500

    Add files via upload

--- matlabassignment_05.m
+++ matlabassignment_05.m
@@ -1,4 +1,10 @@
 imp_op = detectImportOptions("C:\Users\woollastoncb\Downloads\SampleWeatherData.csv");
 imp_op.SelectedVariableNames = {'DateTime','Temperature','RH','WindSpeed','WindDirection','WeatherConditions'};
 
-tab = readtable('SampleWeatherData.csv',imp_op);
+% tab = readtable('SampleWeatherData.csv',imp_op);
+
+imp_op = setvartype(imp_op,{'WindDirection','WeatherConditions'},'categorical');
+imp_op = setvaropts(imp_op,{'Temperature','RH','WindSpeed'},"FillValue",nan);
+imp_op = setvaropts(imp_op,{'WindDirection','WeatherConditions'},"FillValue",'No Data');
+
+readtimetable('C:\Users\woollastoncb\Downloads\SampleWeatherData.csv',imp_op);
